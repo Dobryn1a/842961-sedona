bd42ca196781e78865325672819f7bb2ea2f4b0d
Dobryn1a <movist00@gmail.com>
2018-09-15 00:36:48 +0700

    Правки скриптов #3

--- js/script.js
+++ js/script.js
@@ -5,19 +5,16 @@ var dateout = modal.querySelector('.form-date-out');
 var adults = modal.querySelector('[name=adults]');
 var form = modal.querySelector('.form');
 
-    btn.addEventListener('click', function(evt) {
+btn.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.toggle('modal-show');
-
-
 });
 
-    modal.addEventListener('submit', function (evt) {
-      if (!datein.value || !dateout.value || !adults.value) {
-      evt.preventDefault();
-      form.classList.remove('error');
-      form.offsetWidth = form.offsetWidth;
-      form.classList.add('error');
-  }
-
+modal.addEventListener('submit', function (evt) {
+    if (!datein.value || !dateout.value || !adults.value) {
+        evt.preventDefault();
+        form.classList.remove('error');
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add('error');
+    }
 });
