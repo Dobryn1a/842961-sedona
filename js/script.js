6650c3909ecdcbd3c4944e0ab2c281e441eb56b8
Dobryn1a <movist00@gmail.com>
2018-09-14 20:52:31 +0700

    Правки скриптов

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-
-    var btn = document.querySelector('.button-search-hotels');
-    var modal = document.querySelector('.form-search-hotels');
+var btn = document.querySelector('.button-search-hotels');
+var modal = document.querySelector('.form-search-hotels');
+var datein = modal.querySelector('.form-date-in');
+var dateout = modal.querySelector('.form-date-out');
+var adults = modal.querySelector('[name=adults]');
+var form = modal.querySelector(".form");
 
     btn.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.toggle('modal-show');
+    datein.focus();
 
-    });
+    form.addEventListener('submit', function (evt) {
+      if (!datein.value || !dateout.value || !adults.value) {
+        evt.preventDefault();
+        form.classList.remove("modal-error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add('modal-error');
+      }
+      });
+});
