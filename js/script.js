8e88cb9b0eef0545e6325c5464c1e6489b2cad9b
Dobryn1a <movist00@gmail.com>
2018-09-14 22:20:06 +0700

    Правки скриптов #2

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ var modal = document.querySelector('.form-search-hotels');
 var datein = modal.querySelector('.form-date-in');
 var dateout = modal.querySelector('.form-date-out');
 var adults = modal.querySelector('[name=adults]');
-var form = modal.querySelector(".form");
+var form = modal.querySelector('.form');
 
     btn.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.toggle('modal-show');
 
-    form.addEventListener('submit', function (evt) {
+
+});
+
+    modal.addEventListener('submit', function (evt) {
       if (!datein.value || !dateout.value || !adults.value) {
-        evt.preventDefault();
-        form.classList.remove('error');
-        form.offsetWidth = form.offsetWidth;
-        form.classList.add('error');
-      }
-    });
+      evt.preventDefault();
+      form.classList.remove('error');
+      form.offsetWidth = form.offsetWidth;
+      form.classList.add('error');
+  }
+
 });
